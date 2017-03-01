79e528e07f2b65bb78d32cc7cdf726d88ccf5a7b
gitwoojohn <excellwork@gmail.com>
2017-03-01 22:55:10 +0900

    Demo Ribbon Custom UX & Windows Install

--- RibbonUX/ThisAddIn.cs
+++ RibbonUX/ThisAddIn.cs
@@ -6,17 +6,23 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
 
 namespace RibbonUX
 {
     public partial class ThisAddIn
     {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        private void ThisAddIn_Startup( object sender, System.EventArgs e )
         {
         }
 
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        private void ThisAddIn_Shutdown( object sender, System.EventArgs e )
         {
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon();
         }
 
         #region VSTO에서 생성한 코드
@@ -27,10 +33,10 @@ namespace RibbonUX
         /// </summary>
         private void InternalStartup()
         {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+            this.Startup += new System.EventHandler( ThisAddIn_Startup );
+            this.Shutdown += new System.EventHandler( ThisAddIn_Shutdown );
         }
-        
+
         #endregion
     }
 }
